b71d1a41ca79c6a9023cf646165bb034090c3373
ejimvincent <ejimovc@gmail.com>
2024-04-24 10:39:25 +0100

    upload

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,3 @@
-# Ensure that Werkzeug is installed as well
-package { 'Werkzeug':
-  ensure   => 'installed',
-  provider => 'pip3',
-}
-
 # Install a specific version of Flask (2.1.0)
 package { 'Flask':
   ensure   => '2.1.0',
