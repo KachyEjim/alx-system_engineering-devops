8a23375a1c5eb0b4c30eb5c3f570e49eadde506c
ejimvincent <ejimovc@gmail.com>
2024-05-21 00:07:50 +0100

    BUILD(0x15-api:Task 1) Records all tasks that are owned by this employee in csv format

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Python script that, using this REST API, for
+a given employee ID, returns
+information about his/her TODO list progress."""
+
+import json
+from sys import argv
+import requests
+
+
+def get_employee_tasks(id):
+    """ Get employee tasks """
+    url = f'https://jsonplaceholder.typicode.com/'
+    users = f'users?id={id}'
+    todos = f'todos?userId={id}'
+    userData = requests.get(f'{url}{users}').json()
+    userName = userData[0].get("username")
+    todosData = requests.get(f'{url}{todos}').json()
+
+    """Export into csv"""
+    with open(f'{id}.json', 'w') as f:
+        data = {id: []}
+        for todo in todosData:
+            temp = {
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": userName,
+                }
+            data[id].append(temp)
+        json.dump(data, f)
+
+
+if __name__ == "__main__":
+    id = argv[1]
+    get_employee_tasks(id)
