f91ccc7726d1c8c2d010b7e72374186b6e595eae
ejimvincent <ejimovc@gmail.com>
2024-05-20 23:04:40 +0100

    BUILD(0x15-api:Task0)A script that uses REST api to return inforation about a give users todo list progress

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Python script that, using this REST API, for
+a given employee ID, returns
+information about his/her TODO list progress."""
+
+import urllib
+import urllib.request
+from sys import argv
+import json
+
+
+def get_employee_tasks(employeeId):
+    """ Get employee tasks """
+    url_todos = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+        employeeId)
+    url_users_name = 'https://jsonplaceholder.typicode.com/users/{}'.format(
+        employeeId)
+    response_todos = urllib.request.urlopen(url_todos).read()
+    response_user = urllib.request.urlopen(url_users_name).read()
+    data_todos = json.loads(response_todos)
+    data_user = json.loads(response_user)
+    
+    tasks = []
+    for task in data_todos:
+        if task.get('completed') is True:
+            tasks.append(task.get('title'))
+    
+    print("Employee {} is done with tasks({}/{}):".format(
+        data_user.get('name'), len(tasks), len(data_todos)))
+    for task in tasks:
+        print("\t {}".format(task))
+
+
+if __name__ == "__main__":
+    id = argv[1]
+    get_employee_tasks(id)
