886b69e32c7044f355c5c7f448e51db8eb13df4d
kachyEjim <ejimovcgmail.com>
2024-06-04 18:31:47 +0100

    upload

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,8 @@ def number_of_subscribers(subreddit):
     the number of subscribers for a given subreddit.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url)
+    headers = {"User-Agent": "0x16-api_advanced/V1"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         subscribers = data["data"]["subscribers"]
