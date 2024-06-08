578d786d76846b7d1dda99769de688cf4410b595
root <kachyejim>
2024-06-08 18:17:33 +0000

    upload

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
-""" How many subscribers? """
+"""
+number of subscribers for a given subreddit
+"""
 
 from requests import get
 
 
 def number_of_subscribers(subreddit):
     """
-    Returns subscriber count of subreddit or 0
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
     """
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-
-    headers = {"user-agent": "Kachytech"}
-
-    r = get(url, headers=headers, allow_redirects=False)
-
-    if r.status_code != 200:
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
 
+    user_agent = {"User-agent": "Google Chrome Version 81.0.4044.129"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
     try:
-        js = r.json()
+        return results.get("data").get("subscribers")
 
     except Exception:
         return 0
-
-    data = js.get("data")
-
-    if data:
-        sub_count = data.get("subscribers")
-        if sub_count:
-            return sub_count
-
-    return 0
