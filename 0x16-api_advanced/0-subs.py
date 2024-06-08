ff18c6afb194e23253f03e1cd8bc372aa619cbe1
root <kachyejim>
2024-06-08 20:01:55 +0000

    upload

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,9 +15,8 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {"User-agent": "Google Chrome Version 81.0.4044.129"}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = get(url)
     if response.status_code != 200:
         return 0
 
