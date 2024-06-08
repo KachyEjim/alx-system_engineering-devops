887ce4f194454c9b569ab779927e0daf39065404
root <EJIMOVC@GMAIL.COM>
2024-06-08 19:55:35 +0000

    upload

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,7 +16,7 @@ def number_of_subscribers(subreddit):
         return 0
 
     user_agent = {"User-agent": "Google Chrome Version 81.0.4044.129"}
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(url, headers=user_agent)
     if response.status_code != 200:
         return 0
